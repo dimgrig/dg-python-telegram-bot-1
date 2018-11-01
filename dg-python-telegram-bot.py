2c8f7a409bc49e75814cc2830e66f331a06d279a
dimgrig <dim_grig@mail.ru>
2018-11-01 17:04:59 +0300

    changes

--- dg-python-telegram-bot.py
+++ dg-python-telegram-bot.py
@@ -1,40 +1,24 @@
-import logging
 import os
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-
-def start(bot, update):
-    update.effective_message.reply_text("Hi!")
-
-
-def echo(bot, update):
-    update.effective_message.reply_text(update.effective_message.text)
-
-def error(bot, update, error):
-    logger.warning('Update "%s" caused error "%s"', update, error)
+def handler(bot, update):
+    update.effective_message.reply_text(update.effective_message.text + 'lol')
 
 
 if __name__ == "__main__":
     # Set these variable to the appropriate values
-    TOKEN = "Your token from @Botfather"
-    NAME = "The name of your app on Heroku"
+    TOKEN = "494073850:AAFSGGw53qxhyyHwVLTJNyVfyjqTiaKoLBg"
+    NAME = "fierce-woodland-12673"
 
     # Port is given by Heroku
     PORT = os.environ.get('PORT')
-
-    # Enable logging
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-    logger = logging.getLogger(__name__)
 
     # Set up the Updater
     updater = Updater(TOKEN)
     dp = updater.dispatcher
     # Add handlers
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(Filters.text, echo))
-    dp.add_error_handler(error)
+    dp.add_handler(MessageHandler(Filters.text, handler))
 
     # Start the webhook
     updater.start_webhook(listen="0.0.0.0",
